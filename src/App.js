f5992817b741b7ca6c32b893182d71842cc45919
mnaofficial <mna.official92@gmail.com>
2022-05-09 23:57:56 +0600

    Routes setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h2>doctors-portal-client-71</h2>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
