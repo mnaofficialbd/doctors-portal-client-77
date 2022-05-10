e2f9d23546a4eb72744523744a4a03a073bf6cfb
mnaofficial <mna.official92@gmail.com>
2022-05-10 11:02:22 +0600

    info and info-card complete

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,11 +6,12 @@ import phone from '../../assets/icons/phone.svg';
 
 
 const Info = () => {
+
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard img={clock} />
-            <InfoCard img={marker} />
-            <InfoCard img={phone} />
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCard cartTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock} />
+            <InfoCard cartTitle="Our Locations" bgClass="bg-accent" img={marker} />
+            <InfoCard cartTitle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone} />
         </div>
     );
 };
