cf968a1eb280ededc3a98782f02d008c147f3552
mnaofficial <mna.official92@gmail.com>
2022-05-10 13:57:31 +0600

    review and appointed setup complete

---
+++ src/Pages/Home/Review.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Review = ({ review }) => {
+    const { description, img, name, location } = review;
+    return (
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div class="card-body">
+                <p>{description}</p>
+                <div className='flex items-center'>
+                    <div class="avatar">
+                        <div class="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
+                            <img src={img} alt='' />
+                        </div>
+                    </div>
+                    <div>
+                        <h4 className='text-xl'>{name}</h4>
+                        <p>{location}</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Review;
--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import quote from '../../assets/icons/quote.svg'
+import people1 from '../../assets/images/people1.png'
+import people2 from '../../assets/images/people2.png'
+import people3 from '../../assets/images/people3.png'
+import Review from './Review';
 
 const Testimonials = () => {
+    const reviews = [
+        {
+            _id: 1,
+            name: 'Winson Herry',
+            description: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            img: people1,
+            location: 'California'
+        },
+        {
+            _id: 2,
+            name: 'Cavity Filling',
+            description: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            img: people2,
+            location: 'California'
+        },
+        {
+            _id: 3,
+            name: 'Teeth Whitening',
+            description: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            img: people3,
+            location: 'California'
+        }
+    ]
     return (
         <section className='my-20'>
             <div className='flex justify-between'>
@@ -13,8 +40,13 @@ const Testimonials = () => {
                     <img className='w-24 lg:w-48' src={quote} alt="" />
                 </div>
             </div>
-            <div>
-
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
             </div>
         </section>
     );
