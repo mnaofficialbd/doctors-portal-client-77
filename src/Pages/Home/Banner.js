199763b4709f8660ef68dfd39163943806209383
mnaofficial <mna.official92@gmail.com>
2022-05-12 00:15:36 +0600

    firebase init

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div className='mx-10'>
                     <h1 className="text-5xl font-bold">Your New Smile Start Here</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
