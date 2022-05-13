a1dcd321b0cd065929d91b14da84ea23d56f0e15
mnaofficial <mna.official92@gmail.com>
2022-05-14 01:30:07 +0600

    file add

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -5,13 +5,23 @@ import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
 
 
-const BookingModal = ({ treatment, date ,setTreatment}) => {
+const BookingModal = ({ treatment, date, setTreatment }) => {
     const { _id, name, slots } = treatment;
     const [user, loading] = useAuthState(auth);
+    const formattedDate = format(date, 'PP');
     const handleBooking = event => {
         event.preventDefault();
         const slot = event.target.slot.value;
-        console.log(_id,slot,name);
+        console.log(_id, slot, name);
+        const booking ={
+            treatmentId: _id,
+            treatment: name,
+            date: formattedDate,
+            slot,
+            patient: user.email,
+            patientName:user.displayName,
+            phone: event.target.phone.value
+        };
 
         //for close the modal
         setTreatment(null)
@@ -31,13 +41,13 @@ const BookingModal = ({ treatment, date ,setTreatment}) => {
                         <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
                         <select name="slot" className="select select-bordered w-full max-w-xs">
                             {
-                                slots.map((slot, index) => <option 
-                                   key={index}
+                                slots.map((slot, index) => <option
+                                    key={index}
                                     value={slot}
-                                    >{slot}</option>)
+                                >{slot}</option>)
                             }
                         </select>
-                        <input type="text" name='name' disabled value={user?.displayName|| ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name='name' disabled value={user?.displayName || ''} className="input input-bordered w-full max-w-xs" />
                         <input type="email" name='email' disabled value={user?.email || ''} className="input input-bordered w-full max-w-xs" />
                         <input type="text" name='phone' placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
                         <input type="submit" value="Submit" className="btn btn-secondary w-full max-w-xs" />
