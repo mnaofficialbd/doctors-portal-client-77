226e52cacbb013277ca981275ea9431ded3cd695
mnaofficial <mna.official92@gmail.com>
2022-05-15 20:12:38 +0600

    Available Appointments setup

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,19 +1,17 @@
-import { format } from 'date-fns';
 import React from 'react';
+import { format } from 'date-fns';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
-import Loading from '../Shared/Loading';
 import { toast } from 'react-toastify';
 
-
-const BookingModal = ({ treatment, date, setTreatment }) => {
+const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
     const { _id, name, slots } = treatment;
-    const [user, loading] = useAuthState(auth);
+    const [user] = useAuthState(auth);
     const formattedDate = format(date, 'PP');
     const handleBooking = event => {
         event.preventDefault();
         const slot = event.target.slot.value;
-        console.log(_id, slot, name);
+
         const booking = {
             treatmentId: _id,
             treatment: name,
@@ -22,7 +20,7 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
             patient: user.email,
             patientName: user.displayName,
             phone: event.target.phone.value
-        };
+        }
 
         fetch('http://localhost:5000/booking', {
             method: 'POST',
@@ -34,18 +32,14 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
             .then(res => res.json())
             .then(data => {
                 if(data.success){
-                    toast(`Appointment is set ${formattedDate} at ${slot}`)
+                    toast(`Appointment is set, ${formattedDate} at ${slot}`)
                 }
                 else{
-                    toast.error(`Already have an appointment on ${data.booking?.date} at ${data.booking?.slot}`)
+                    toast.error(`Already have and appointment on ${data.booking?.date} at ${data.booking?.slot}`)
                 }
-                //for close the modal
-                setTreatment(null)
-            })
-
-    }
-    if (loading) {
-        return <Loading />
+                setTreatment(null);
+                refetch();
+            });
     }
 
     return (
@@ -55,7 +49,7 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
                 <div className="modal-box">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg text-secondary">Booking for: {name}</h3>
-                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-4 justify-items-center mt-4'>
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
                         <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
                         <select name="slot" className="select select-bordered w-full max-w-xs">
                             {
@@ -65,9 +59,9 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
                                 >{slot}</option>)
                             }
                         </select>
-                        <input type="text" name='name' disabled value={user?.displayName || ''} className="input input-bordered w-full max-w-xs" />
-                        <input type="email" name='email' disabled value={user?.email || ''} className="input input-bordered w-full max-w-xs" />
-                        <input type="text" name='phone' placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="name" disabled value={user?.displayName || ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="email" name="email" disabled value={user?.email || ''} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
                         <input type="submit" value="Submit" className="btn btn-secondary w-full max-w-xs" />
                     </form>
                 </div>
