ede68570725e57097568eab529552e31cd0aaeee
mnaofficial <mna.official92@gmail.com>
2022-05-16 00:27:54 +0600

    token create

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 const SignUp = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -15,6 +16,7 @@ const SignUp = () => {
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
     const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+    const [token]=useToken(user || gUser)
     const navigate=useNavigate();
     let signInError;
 
@@ -26,15 +28,15 @@ const SignUp = () => {
         signInError = <p className='text-red-600'> <small>{error?.message || gError?.message || updateError?.message}</small> </p>
     }
 
-    if (user || gUser) {
+    if (token) {
         console.log(user || gUser);
+        navigate('/appointment');
     }
 
     const onSubmit = async data => {
         console.log(data);
         await createUserWithEmailAndPassword(data.email, data.password);
         await updateProfile({ displayName: data.name });
-        navigate('/appointment');
     };
 
 
