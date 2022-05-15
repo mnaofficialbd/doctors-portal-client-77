3b30cc399abee3ff31b1fbede7f780822bab0b4b
mnaofficial <mna.official92@gmail.com>
2022-05-15 22:05:09 +0600

    My appointment data display

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import MyAppointments from './Pages/Dashboard/MyAppointments';
 import MyReview from './Pages/Dashboard/MyReview';
+import MyHistory from './Pages/Dashboard/MyHistory';
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
         } >
           <Route index element={<MyAppointments />} />
           <Route path='review' element={<MyReview />} />
+          <Route path='history' element={<MyHistory />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,20 +4,18 @@ import { Link, Outlet } from 'react-router-dom';
 const Dashboard = () => {
     return (
         <div class="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col items-center justify-center">
-                <h2 className='text-5xl text-purple-500'>Dashboard</h2>
+            <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content">
+                <h2 className='text-2xl font-bold text-purple-500'>Welcome to Dashboard</h2>
                 <Outlet/>
-                {/* <!-- Page content here --> */}
-                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
             </div>
             <div class="drawer-side">
-                <label for="my-drawer-2" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                <label for="dashboard-sidebar" class="drawer-overlay"></label>
+                <ul class="menu p-4 overflow-y-auto w-47 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><Link to='/dashboard'>Sidebar Item 1</Link></li>
-                    <li><Link to='/dashboard/review'>Sidebar Item 2</Link></li>
+                    <li><Link to='/dashboard'>My Appointments</Link></li>
+                    <li><Link to='/dashboard/review'>My Reviews</Link></li>
+                    <li><Link to='/dashboard/history'>My History</Link></li>
                 </ul>
 
             </div>
