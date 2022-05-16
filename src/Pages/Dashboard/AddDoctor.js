d0824dd404695c4b0267266794b56b470f976bd9
mnaofficial <mna.official92@gmail.com>
2022-05-17 01:13:24 +0600

    add doctor form setup

--- src/Pages/Dashboard/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 
 
 const AddDoctor = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
 
+    const { data: services, isLoading } = useQuery('services', () => { fetch('http://localhost:5000/service').then(res => res.json()) })
+
     const onSubmit = async data => {
         console.log(data);
+    };
+
+    if (isLoading) {
+        return <Loading />
     };
 
     return (
@@ -54,19 +62,34 @@ const AddDoctor = () => {
                     <label className="label">
                         <span className="label-text">Specialty</span>
                     </label>
-                    <input type="text" placeholder="specialty"
+
+                    <select {...register("specialty")} class="select w-full max-w-xs">
+                        {
+                            services?.map(service=><option
+                            key={service._id}
+                            value={service.name}
+                            >{service.name}</option>)
+                        }
+                    </select>
+                </div>
+
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Photo</span>
+                    </label>
+                    <input type="file"
                         className="input input-bordered w-full max-w-xs"
-                        {...register("specialty", {
+                        {...register("image", {
                             required: {
                                 value: true,
-                                message: 'Specialization is Required'
+                                message: 'Image is Required'
                             }
                         })} />
                     <label className="label">
-                        {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
-                        {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
                     </label>
                 </div>
+
                 <input className='btn w-full max-w-xs text-white' type="submit" value='Add' />
             </form>
         </div>
